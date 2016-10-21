4633ffd01ab4ad01bfb2d1b1fd9d489801f8abed
aminhp93 <minhpn.org.ec@gmail.com>
2016-10-20 17:28:38 -0700

    asdf

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './client')));
 app.use(express.static(path.join(__dirname, './bower_components')));
@@ -12,6 +12,6 @@ app.use(express.static(path.join(__dirname, './bower_components')));
 require('./server/config/mongoose.js');
 require('./server/config/routes.js')(app);
 
-app.listen(3000, function(){
-	console.log('LOGIN AND REGISTRATION on port 3000');
+app.listen(8000, function() {
+    console.log('LOGIN AND REGISTRATION on port 3000');
 })
